8671f0b51b5f71d850627ef7d913b388a38b7a66
Kali66ty <kalisnomaa@gmail.com>
2025-11-02 00:09:10 +0100

    Add files via upload

---
+++ pages.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '100vh',
+      fontFamily: 'Arial, sans-serif',
+      flexDirection: 'column'
+    }}>
+      <h1>Welcome to Mubarak Barade Data</h1>
+      <p>Your reliable platform for buying data and airtime.</p>
+    </div>
+  );
+}
