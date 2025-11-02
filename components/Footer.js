0ccab7cba9abe33c5df784e753e331b8a177f837
Kali66ty <kalisnomaa@gmail.com>
2025-11-02 01:17:05 +0100

    Add Footer component with copyright notice

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full text-center py-4 bg-gray-800 text-white mt-16">
+      © {new Date().getFullYear()} Mubarak Barade Data. All rights reserved.
+    </footer>
+  );
+    }
